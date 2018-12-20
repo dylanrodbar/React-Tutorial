979749b770a22aebc71062e338e51ecdc09d96c3
dylan.rodriguez <dylan.rodriguez@avantica.net>
2018-12-20 11:36:33 -0600

    lecture 45

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -13,14 +13,23 @@ class App extends Component {
     otherState: 'some other value'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //DON'T DO THIS this.state.persons[0].name="Joanne";
     this.setState({persons: [
-      { name: 'Lady Gaga', age: 21},
-      { name: 'Jhoel', age: 23},
+      { name: newName, age: 21},
+      { name: 'Jhoel', age: 24},
       { name: 'Stephannie', age: 25},
     ]
-  });
+  })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({persons: [
+      { name: 'Dylan', age: 21},
+      { name: event.target.value, age: 24},
+      { name: 'Stephannie', age: 25},
+    ]
+  })
   }
 
   render() {
@@ -28,10 +37,18 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Hello from the other side</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hello from the other side</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Hello from the other side</Person>
+        <button onClick={() => this.switchNameHandler('Mary Jane Holland')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}>Hello from the other side</Person>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={() => this.switchNameHandler('Alejandro')}
+          changed={this.nameChangedHandler}>Hello from the other side</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}>Hello from the other side</Person>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
