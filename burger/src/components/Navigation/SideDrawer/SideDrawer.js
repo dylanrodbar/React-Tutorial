d674b4e726ff3afbe629b602c664c813e6d5c8bb
dylanrodbar <dylanrodbar97@gmail.com>
2018-12-29 03:15:30 -0600

    lecture 140

--- burger/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,17 +3,25 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Auxiliary from '../../../hoc/Auxiliary';
 
 const sideDrawer = (props) => {
-    
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if(props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
     return(
-        <div className={classes.SideDrawer}>
-            <Logo height="11%"/>
-            <nav>
-                <NavigationItems />
-            </nav>
-            
-        </div>
+        <Auxiliary>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <Logo height="11%"/>
+                <nav>
+                    <NavigationItems />
+                </nav>
+                
+            </div>
+        </Auxiliary>
     );
 };
 
