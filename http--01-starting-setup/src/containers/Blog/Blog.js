d81f3dac06a1b07149d1cf9881f9071e1d82ee43
dylanrodbar <dylanrodbar97@gmail.com>
2019-01-03 11:13:35 -0600

    186

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 import { Route } from 'react-router-dom'
 //import axios from 'axios';
@@ -25,9 +26,11 @@ class Blog extends Component {
                     </nav>
 
                 </header>
-                <Route path="/" exact render={() => <h1>Home</h1>}/>
-                <Route path="/" render={() => <h1>Home 2</h1>}/>
-                    <Posts/>
+                
+                {/*<Route path="/" exact render={() => <h1>Home</h1>}/>
+                <Route path="/" render={() => <h1>Home 2</h1>}/>*/}
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
                 
             </div>
         );
