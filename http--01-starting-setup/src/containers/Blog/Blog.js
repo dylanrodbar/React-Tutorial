171aed30fab46219efd4f7e18c6ab3153e6957c2
dylanrodbar <dylanrodbar97@gmail.com>
2019-01-03 11:06:37 -0600

    184

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,52 +1,19 @@
 import React, { Component } from 'react';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import Posts from './Posts/Posts';
 
+import { Route } from 'react-router-dom'
 //import axios from 'axios';
 import axios from '../../axios';
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
+    
 
-    componentDidMount () {
-        axios.get('/posts')
-            .then(response => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Dylan'
-                    }
-                });
-                this.setState({posts: updatedPosts});
-            })
-            .catch(error => {
-                this.setState({error: true});
-            });
-    }
+    
 
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-    }
+    
 
     render () {
-        let posts = <p style={{textAlign:'center'}}>Something went wrong</p>
-        if(!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)}/>
-            });
-        }
-        
         return (
             <div className="Blog">
                 <header>
@@ -58,15 +25,10 @@ class Blog extends Component {
                     </nav>
 
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                <Route path="/" exact render={() => <h1>Home</h1>}/>
+                <Route path="/" render={() => <h1>Home 2</h1>}/>
+                    <Posts/>
+                
             </div>
         );
     }
