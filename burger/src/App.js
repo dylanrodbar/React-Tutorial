f99754248f2cc54ce92870614f00e00dfff7b392
dylan.rodriguez <dylan.rodriguez@avantica.net>
2019-01-04 15:29:58 -0600

    lecture 218

--- burger/src/App.js
+++ burger/src/App.js
@@ -2,23 +2,20 @@ import React, { Component } from 'react';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import Checkout from './containers/Checkout/Checkout';
+
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
-  state = {
-    show: true
-  };
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({show: false});
-
-    }, 5000)
-  }
+  
   render() {
     return (
       <div>
         <Layout>
-          {this.state.show ? <BurgerBuilder></BurgerBuilder> : null}
+          <Switch>
+            <Route path="/" exact component={BurgerBuilder}></Route>
+            <Route path="/checkout" component={Checkout}></Route>
+          </Switch>
           
         </Layout>
         
