9c74acc806b42e546dd0528be90622140938eb4f
dylan.rodriguez <dylan.rodriguez@avantica.net>
2019-01-31 16:30:15 -0600

    lesson 279

--- redux--assignment-1-solution/src/index.js
+++ redux--assignment-1-solution/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 import counter from './store/reducers/counter';
 import result from './store/reducers/result';
@@ -30,7 +31,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
