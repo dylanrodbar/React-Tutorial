7d52107d6f69e1ca6fa437492f2c49bb322d0938
dylan.rodriguez <dylan.rodriguez@avantica.net>
2019-01-31 17:52:53 -0600

    lesson 284

--- redux--assignment-1-solution/src/store/reducers/result.js
+++ redux--assignment-1-solution/src/store/reducers/result.js
@@ -6,17 +6,20 @@ const initialState = {
     results: []
 };
 
+
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter((result, index) => result.id !== action.resultElId);
+    return  updateObject(state, {results: updatedArray}); 
+}; 
+
 const result = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
         return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result * 2})});
         
         case actionTypes.DELETE_RESULT:
-            const updatedArray = state.results.filter((result, index) => result.id !== action.resultElId);
-            return updateObject(state, {results: updatedArray});
+            return deleteResult(state, action);
         
-            
-
     }
     
     return state;
